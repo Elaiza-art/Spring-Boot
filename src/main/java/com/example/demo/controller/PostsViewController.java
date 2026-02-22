9142d9cd51cb3c5cf6d96f37c0e9dc3670b12396
Elaiza-art <liza.kupidonli25@mail.ru>
2026-02-22 20:27:00 +0500

    posts

--- src/main/java/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/controller/PostsViewController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +19,13 @@ public class PostsViewController {
 //        return "Здесь будет главная страница";
 //    }
 
+    @Autowired
+    PostService postsService;
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String list(Model model) {
         model.addAttribute("appName", "Моё супер приложение");
+        model.addAttribute("posts", postsService.listAllPosts());
         return "list";
     }
 

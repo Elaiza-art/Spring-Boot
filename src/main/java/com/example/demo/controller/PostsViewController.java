141678b79659fb9a6bfceaafecfd5ec5dfaa5a54
Elaiza-art <liza.kupidonli25@mail.ru>
2026-02-22 18:03:18 +0500

    view

--- src/main/java/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/controller/PostsViewController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,10 +10,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 
 public class PostsViewController {
-    @ResponseBody
+
+//    @ResponseBody
+//    @RequestMapping(path = "/", method = RequestMethod.GET)
+//    public String list() {
+//        return "Здесь будет главная страница";
+//    }
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
+    public String list(Model model) {
+        model.addAttribute("appName", "Моё супер приложение");
+        return "list";
     }
 
     @ResponseBody
@@ -20,4 +28,5 @@ public class PostsViewController {
     public String single(@PathVariable("id") Long id) {
         return "Здесь будет страница поста №" + id;
     }
+
 }
